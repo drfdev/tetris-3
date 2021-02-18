4f5d8c15e13d6b2ebfa82385ca5114cc2ce353b0
drfdev <drafff@gmail.com>
2021-02-18 23:10:14 +0300

    Bugfix: fix jar building

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ TARGET_DIRECTORY="tetris-3-jar/target"
 if [ -d "$TARGET_DIRECTORY" ]; then
   # target directory exists
   cd $TARGET_DIRECTORY
-  java -jar tetris-3-jar-1.0-SNAPSHOT.jar
+  java -jar tetris-3-jar-1.0-SNAPSHOT-jar-with-dependencies.jar
 else
   # no directory
   echo "Directory: ${TARGET_DIRECTORY} not exists, build project before run"
