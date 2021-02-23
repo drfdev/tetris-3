c0c0736d31bac8d0dab88e904b1ba3e8d5550aeb
drfdev <drafff@gmail.com>
2021-02-23 19:52:05 +0300

    Feature: add lazy initialization to RetailUtil

--- tetris-3-jar/src/main/kotlin/dev/drf/tetris3/jar/Main.kt
+++ tetris-3-jar/src/main/kotlin/dev/drf/tetris3/jar/Main.kt
@@ -1,12 +1,12 @@
 package dev.drf.tetris3.jar
 
 import dev.drf.tetris3.retail.Game
-import dev.drf.tetris3.retail.createGame
+import dev.drf.tetris3.retail.getTetrisGame
 
 fun main() {
     println("!! Tetris !!")
 
-    val tetris: Game = createGame()
+    val tetris: Game = getTetrisGame()
 //    tetris.runGame()
 //    tetris.stopGame()
 }
--- tetris-3-retail/src/main/kotlin/dev/drf/tetris3/retail/RetailUtil.kt
+++ tetris-3-retail/src/main/kotlin/dev/drf/tetris3/retail/RetailUtil.kt
@@ -4,10 +4,16 @@ import dev.drf.tetris3.common.Score
 import dev.drf.tetris3.retail.chain.StartupGameChain
 import dev.drf.tetris3.retail.game.TetrisGame
 
+object RetailUtil {
+    val tetrisGame: TetrisGame by lazy {
+        TetrisGame(StartupGameChain())
+    }
+}
+
 fun getScope(rowCount: Int): Score {
     return Score.scoreByRow(rowCount)
 }
 
-fun createGame(): Game {
-    return TetrisGame(StartupGameChain())
+fun getTetrisGame(): Game {
+    return RetailUtil.tetrisGame
 }
